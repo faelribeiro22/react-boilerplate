f6bd1474b44e7a8d83ef51a745d3e168f94cccfc
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2019-07-23 17:35:24 -0300

    create react boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>WELCOME TO NOTHING 🤗</header>
+    </div>
+  )
+}
+
+export default App;
